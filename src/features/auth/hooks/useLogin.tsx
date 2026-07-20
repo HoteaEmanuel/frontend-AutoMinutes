6c3b30ec2f1c79dcdfa5091460fa9190e707f62b
Lucian-Alexandru Vilvoi <vilvoilucian@gmail.com>
2026-07-20 21:58:37 +0300

    fix: set session and redirect after successful login

--- src/features/auth/hooks/useLogin.tsx
+++ src/features/auth/hooks/useLogin.tsx
@@ -1,12 +1,21 @@
 import { useMutation } from '@tanstack/react-query';
 import { login } from '../api/auth.api';
 import { toast } from 'sonner';
+import { useNavigate } from 'react-router';
+import { useAuthStore } from '../stores/auth.store';
 
 const useLogin = () => {
+  const navigate = useNavigate();
+  const setSession = useAuthStore((auth) => auth.setSession);
+
   return useMutation({
     mutationFn: login,
     mutationKey: ['login'],
-    onSuccess: () => toast.success('Logged in successfully'),
+    onSuccess: ({ data }) => {
+      setSession({ accessToken: data.accessToken, user: data.user });
+      toast.success('Logged in successfully');
+      navigate('/meetings');
+    },
   });
 };
 
